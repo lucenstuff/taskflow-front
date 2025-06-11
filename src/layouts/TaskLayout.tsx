a9b1259673c080a7fac5406d60f430980e30ba7f
lucenstuff <lucentiniagustin@hotmail.com>
2025-06-11 16:37:24 -0300

    refactor(TaskLayout): improve code readability and add mobile menu toggle functionality

--- src/layouts/TaskLayout.tsx
+++ src/layouts/TaskLayout.tsx
@@ -1,20 +1,24 @@
 import { Sidebar } from "@/components/Sidebar";
 import { useState } from "react";
-//funcion flecha 
-export const TaskLayout = ({children}:{children: React.ReactNode})=>{
+import type { ReactNode } from "react";
+
+export const TaskLayout = ({ children }: { children: ReactNode }) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const  handleMobileMenuClose = () => setIsMobileMenuOpen(false);
+  const handleMobileMenuClose = () => setIsMobileMenuOpen(false);
+  const handleMobileMenuToggle = () => setIsMobileMenuOpen((prev) => !prev);
   const handleLogout = () => console.log("logout");
+
   return (
     <>
       <Sidebar
         isMobileMenuOpen={isMobileMenuOpen}
         onMobileMenuClose={handleMobileMenuClose}
+        onMobileMenuToggle={handleMobileMenuToggle}
         brandName="Task Flow"
         onLogout={handleLogout}
       />
       {children}
     </>
   );
-} 
+};
