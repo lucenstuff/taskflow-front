a080a8230100fdbf287a6da1bb897e24b523af3d
lucenstuff <lucentiniagustin@hotmail.com>
2025-06-11 15:50:15 -0300

    feat: add sonner for toast notifications and update App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "sonner";
 import { AuthProvider } from "./contexts/AuthContext";
-import {AppRoutes} from "./Routes";
+import { AppRoutes } from "./Routes";
 
 function App() {
   return (
     <AuthProvider>
+      <Toaster />
       <AppRoutes />
     </AuthProvider>
   );
