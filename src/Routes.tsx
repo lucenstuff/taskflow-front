7a64b86495012f082b0ecadfca49f00c2f0455cf
lucenstuff <lucentiniagustin@hotmail.com>
2025-06-11 16:37:28 -0300

    style(Routes): improve code formatting and readability

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,20 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import AuthLayout from "./layouts/AuthLayout";
 import Login from "./components/Login";
-import {TaskLayout} from "./layouts/TaskLayout";
+import { TaskLayout } from "./layouts/TaskLayout";
 import GroupTaskUpcoming from "./components/GroupTaskUpcoming";
-import TaskListView from "./components/TaskListView";
 import Register from "./components/Register";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route element={<AuthLayout />}>
-        <Route path="/login" element={<Login />}/>
+        <Route path="/login" element={<Login />} />
       </Route>
-      <Route path="/"  element={<TaskLayout children={<GroupTaskUpcoming />} />}></Route>
-      <Route path="/today"  element={<TaskLayout children={<TaskListView />} />}></Route>
-      <Route path="/register" element={<Register/>}></Route>
+      <Route
+        path="/"
+        element={<TaskLayout children={<GroupTaskUpcoming />} />}
+      ></Route>
+      <Route path="/register" element={<Register />}></Route>
     </Routes>
   );
 }
