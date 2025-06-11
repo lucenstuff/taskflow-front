6189ac068d751b3c9b68f6a45a5855250f628349
lucenstuff <lucentiniagustin@hotmail.com>
2025-06-11 17:34:55 -0300

    feat(TaskLayout): implement logout functionality and improve mobile menu handling style(tagService): standardize string quotes and add missing newline at end of file fix(tag): add missing color property to TagDTO and Tag interfaces

--- src/layouts/TaskLayout.tsx
+++ src/layouts/TaskLayout.tsx
@@ -1,13 +1,20 @@
 import { Sidebar } from "@/components/Sidebar";
 import { useState } from "react";
 import type { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const TaskLayout = ({ children }: { children: ReactNode }) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
+  const navigate = useNavigate();
+
   const handleMobileMenuClose = () => setIsMobileMenuOpen(false);
   const handleMobileMenuToggle = () => setIsMobileMenuOpen((prev) => !prev);
-  const handleLogout = () => console.log("logout");
+
+  const handleLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
 
   return (
     <>
--- src/types/tag.ts
+++ src/types/tag.ts
@@ -3,12 +3,14 @@ import type { Task } from './task';
 export interface TagDTO {
   id?: number;
   name: string;
+  color?: string;
   task_id?: number;
 }
 
 export interface Tag {
   id?: number;
   name: string;
+  color?: string;
   createdAt: string;
   updatedAt: string;
   tasks: Task[];
