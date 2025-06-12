7c2c9269a4eadcc3cdad4ab26e19a42b04ecdfc1
lucenstuff <lucentiniagustin@hotmail.com>
2025-06-12 12:41:25 -0300

    fix(NotFound): remove unused imports and update navigation button

--- src/errors/Not_Found.tsx
+++ src/errors/Not_Found.tsx
@@ -1,27 +1,24 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <section className="relative z-10 bg-red-600 min-h-screen flex items-center py-[120px]">
+      <section className="relative min-h-screen flex items-center py-[120px]">
         <div className="container mx-auto">
           <div className="-mx-4 flex justify-center">
             <div className="w-full px-4 max-w-md text-center">
-              <h2 className="mb-2 text-[50px] font-bold leading-none text-white sm:text-[80px] md:text-[100px]">
+              <h2 className="mb-2 text-[50px] font-bold leading-none sm:text-[80px] md:text-[100px]">
                 404
               </h2>
-              <h4 className="mb-3 text-[28px] font-semibold leading-tight text-white">
+              <h4 className="mb-3 text-[28px] font-semibold leading-tighte">
                 Oops! Página no encontrada
               </h4>
-              <p className="mb-8 text-[20px] text-white">
+              <p className="mb-8 text-[20px]">
                 La página que busca no existe o no esta disponible
               </p>
-              <a
-                href="/"
-                className="inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-white hover:text-red-600"
-              >
-                Volver al Login
-              </a>
+              <Button onClick={() => navigate(-1)}>Volver</Button>
             </div>
           </div>
         </div>
